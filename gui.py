256e3d03f208a8afa2e27f4108d5b7cd4b8ed46e
Francis Pandey <pandeyfrancis16g@example.com>
2025-10-22 21:39:09 +0545

    ulu

--- gui.py
+++ gui.py
@@ -1,16 +1,19 @@
 from tkinter import *
+import customtkinter
+
 
 window = Tk()
 window.geometry("420x420")
 window.title("Tip Calculator")
-
+customtkinter.set_appearance_mode("dark")
 
 def tip():
     a=int(amount_entry.get())
     b=(tip_entry.get())
     if b.endswith("%"):
         b=int(b[:-1])
-    
+    else:
+        b=int(b)
     t=(b/100)*a
     print(f"The tip is {t}")
 
