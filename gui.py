3291c683ec80db2f242a6ea8f8c9d58570ab48ab
Francis Pandey <pandeyfrancis16g@example.com>
2025-10-22 19:19:53 +0545

    initial commit

---
+++ gui.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Tip Calculator")
+
+
+def tip():
+    a=int(amount_entry.get())
+    b=(tip_entry.get())
+    if b.endswith("%"):
+        b=int(b[:-1])
+    
+    t=(b/100)*a
+    print(f"The tip is {t}")
+
+
+
+Label(window, text="Enter amount:").pack()
+amount_entry = Entry(window)
+amount_entry.pack()
+
+Label(window, text="Enter tip percentage:").pack()
+tip_entry = Entry(window)
+tip_entry.pack()
+
+Button(window, text="Calculate", command=tip).pack()
+
+result_label = Label(window, text="")
+result_label.pack()
+window.mainloop()
---
+++ tip.py
@@ -0,0 +1,5 @@
+name=["Frnaics","Luis","dino"]
+print(type(name))
+for i in name:
+    print(i,end=" ")
+print(name[0])
